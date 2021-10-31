8f734dc9afee6f22a3d6136b9618c3e3b492bfb3
Wilson <wilson052864@gmail.com>
2021-11-01 00:33:50 +0800

    Update Company_v2 project

--- Company_v2/src/test/java/com/company2/Company_v2/CompanyV2ApplicationTests.java
+++ Company_v2/src/test/java/com/company2/Company_v2/CompanyV2ApplicationTests.java
@@ -2,7 +2,7 @@ package com.company2.Company_v2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootTest
 class CompanyV2ApplicationTests {
         
